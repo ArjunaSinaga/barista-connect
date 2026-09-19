0ea978b1e8d02fff34b51e4ee03454fac839fa8f
Barista Connect <barista@connect.local>
2026-09-19 15:08:31 +0700

    fix CSP: fastly.picsum.photos (redirect picsum)

--- next.config.mjs
+++ next.config.mjs
@@ -18,7 +18,7 @@ const nextConfig = {
           {
             key: "Content-Security-Policy",
             value:
-              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https://*.supabase.co https://randomuser.me https://picsum.photos; connect-src 'self' https://*.supabase.co https://api.midtrans.com https://api.sandbox.midtrans.com wss://*.supabase.co;",
+              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https://*.supabase.co https://randomuser.me https://picsum.photos https://fastly.picsum.photos; connect-src 'self' https://*.supabase.co https://api.midtrans.com https://api.sandbox.midtrans.com wss://*.supabase.co;",
           },
         ],
       },
